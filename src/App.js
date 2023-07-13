d4a455cacfa4eecf16f23245500d4bebf7f6fb5f
gurudascs <gurudascs@gmail.com>
2023-07-13 13:01:17 +0530

    simple-interest-calculator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+
+  //state creation
+  const [principle, setPrinciple] = useState(0)
+  const [rate, setRate] = useState(0)
+  const [year, setYear] = useState(0)
+  const [interest, setInterest] = useState(0)
+
+  console.log(principle);
+  console.log(rate);
+  console.log(year);
+
+  const handleCalculate=(e)=>{
+    let output = principle*year*rate/100
+    console.log(output);
+    setInterest(output)
+  }
+
+  const handleReset=(e)=>{
+    setPrinciple(0)
+    setRate(0)
+    setYear(0)
+    setInterest(0)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="head">
+            <h1>Simple Interest Calculator</h1>
+            <p>The Simple Interest Calculator calculates the interest and end balance based on the simple interest formula. Click the tabs to calculate the different parameters of the simple interest formula. In real life, most interest calculations involve compound Interest. To calculate compound interest, use the Interest Calculator.</p>
+          </div>
+
+          <div className="card">
+            <h3 className='symbol'>&#8377;{interest}</h3>
+            <p id='para'>Your Total Simple Interest</p>
+          </div>
+
+          <div className='input'>
+            <TextField id="outlined-basic" label="Principle Amount" variant="outlined" 
+              onChange={e=>setPrinciple(e.target.value)} value={principle}
+            />
+            <br />
+            <TextField id="outlined-basic" label="Rate of Interest" variant="outlined" 
+              onChange={e=>setRate(e.target.value)} value={rate}
+            />
+            <br />
+            <TextField id="outlined-basic" label="Year" variant="outlined" 
+              onChange={e=>setYear(e.target.value)} value={year}
+            />
+
+            <br />
+            <Button variant="contained"
+              onClick={handleCalculate}
+            >Calculate</Button>
+            <br />
+            <Button variant="outlined"
+              onClick={handleReset}
+            >Reset</Button>
+          </div>
+
+
+
+        </div>
       </header>
     </div>
   );
